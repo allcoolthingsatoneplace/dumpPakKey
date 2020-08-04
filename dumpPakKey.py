bf78706c2f567f7c99a182546c271cd68e407ec3
somethingcoolmustbehere <68785020+somethingcoolmustbehere@users.noreply.github.com>
2020-08-04 18:08:25 +0300

    dumpPakKey [tool]
    initial commit

---
+++ dumpPakKey.py
@@ -0,0 +1,114 @@
+import re
+import sys
+import codecs
+import binascii
+import os
+
+## Pattern section
+initPattern = b'\x48\x8D\x41\x1F\xC7\x45\xD8'
+junkPattern = ["c745d4", "488d411fc745d8", "c745dc", "c745e0", "c745e4", "c745e8", "c745ec"]
+##
+
+## Array section
+offsetArray = []
+dumpKeySectionList = []
+##
+
+## Error section
+errorFile = "[!] Please provide a filename to proceed."
+errorData = "[!] File specified doesn't exist, please proceed a valid file."
+errorOffset = "[!] Something went wrong, no offset value has been set."
+errorDumpKey = "[!] Something went wrong, can't dump bytes to array."
+errorGenKey = "[!] Something went wrong, can't generate key."
+##
+
+## Function section
+
+def incrementOffset(offset, jumpBytes):
+	for i in range(0, jumpBytes):
+		i = int(offset, 16)
+		i +=1                   
+		offset = hex(i)
+		offsetArray.append(offset)
+
+def decrementOffset(offset, jumpBytes):
+	for i in range(0, jumpBytes):
+		i = int(offset, 16)
+		i -=1                 
+		offset = hex(i)
+		offsetArray.append(offset)
+
+def DumpKeyBlock():
+	for offset in offsetArray:
+		offset = int(offset, 16)
+		f.seek(offset, 0)
+		keyData = f.read(1)
+		keyData = binascii.hexlify(keyData)
+		dumpKeySectionList.append(keyData.decode('utf-8'))
+
+def cleanGenKey(junkHex, hexBlock):
+	cleanHex = hexBlock
+	for trash in junkHex:
+		cleanHex = re.sub(trash, '', cleanHex)
+
+	print ("[i] Pure [hex] key extracted: " + cleanHex)
+
+	b64 = codecs.encode(codecs.decode(cleanHex, 'hex'), 'base64').decode()
+
+	print("[i] Base64 Decription *.pak key: " + b64)
+
+##
+
+## argv contains data?!
+if len(sys.argv) > 1:
+
+	fileName = sys.argv[1]
+	filePath = os.path.exists(fileName)
+
+	## Checking if file in argv exists, exec.
+	if filePath:
+		
+		f = open(fileName, "rb")
+		data = f.read()
+		regex = re.compile(initPattern)
+		for match_obj in regex.finditer(data):
+			offset = match_obj.start()
+
+			print ("[i] Pattern match found, our initial offset is: " + hex(offset))
+
+			offset = hex(offset)
+			## Add manualy initial offset to array
+			offsetArray.append(offset)
+
+			## Getting offset section 47 bytes(where are key stored), up and down from initial match 46 + initial offset = 47
+			incrementOffset(offset, 45)
+			decrementOffset(offset, 11)
+
+			## Sortnig your offsets by increase (dafault sort option)
+			offsetArray.sort()
+		
+			print("[i] Retreaving key offset section.")
+			print(*offsetArray)
+
+		if offsetArray:
+			DumpKeyBlock()
+			f.close()
+
+			print("[i] Retreaving key block section.")
+			print(*dumpKeySectionList)
+		else:
+
+			print(errorDumpKey)
+
+		if dumpKeySectionList:
+			## Generating base64 key from sorted HEX block
+			hex = ''.join(dumpKeySectionList)
+			## Filtering junk from key block and Gen key
+			cleanGenKey(junkPattern, hex)
+
+		else:
+			print(errorGenKey)
+	else:
+		print(errorData)		
+else:
+	print(errorFile)
